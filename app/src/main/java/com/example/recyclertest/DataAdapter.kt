a659f639819d79989f19f2ec89ec66b6a583d9cf
K-HUB  LATITUDE <borebrian77@gmai.com>
2020-02-03 22:15:49 -0800

    v2

---
+++ app/src/main/java/com/example/recyclertest/DataAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.recyclertest
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class DataAdapter:RecyclerView.Adapter<DataAdapter.MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+}
+
+
+}
---
+++ app/src/main/java/com/example/recyclertest/Model.kt
@@ -0,0 +1,13 @@
+package com.example.recyclertest
+
+data class Hobby(var title:String)
+
+object Supplier{
+    val hobbies= listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Stonning")
+
+    )
+
+}
